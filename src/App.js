4ceb05cb0638304dd245f9aa545ba4dd8edf6bd0
Philipp Droste <philipp.droste@yahoo.com>
2024-05-22 19:54:13 -0700

    React rewrite

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Articles from './pages/Articles';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/articles" element={<Articles />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Articles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Articles = () => {
+  return (
+    <div className="container text-center">
+      <h1>Articles</h1>
+      <p>Some articles...</p>
+    </div>
+  );
+};
+
+export default Articles;
