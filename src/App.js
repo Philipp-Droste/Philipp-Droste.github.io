ba4987fc291e6eac2c457053b7cab6703ed54c8a
Philipp Droste <philipp.droste@yahoo.com>
2024-06-01 03:51:31 -0700

    hovering and more

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Articles from './pages/Articles.jsx';
+import MarkdownPage from './pages/MarkdownPage.jsx';
+import { articleTypes } from './components/article_card_list/functions.js';
+
+export async function getArticleDirectories(articleType) {
+  const response = await fetch(`/articles/${articleTypes[articleType]}/directories.json`, {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+  });
+
+  return await response.json();
+}
 
 const App = () => {
+  const [blogArticleDirectories, setBlogArticleDirectories] = useState([]);
+  const [projectArticleDirectories, setProjectArticleDirectories] = useState([]);
+
+  useEffect(() => {
+      async function loadData() {
+        const retrievedBlogArticleDirectories = await getArticleDirectories('blog');
+        const retrievedProjectArticleDirectories = await getArticleDirectories('blog');
+        setBlogArticleDirectories(retrievedBlogArticleDirectories);
+        setProjectArticleDirectories(retrievedProjectArticleDirectories);
+      }
+      loadData();
+  }, []);
+
   return (
     <Router basename={`${process.env.PUBLIC_URL}`}>
       <div>
@@ -14,8 +40,16 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Articles />} />
+          <Route path="/projects" element={<Projects />}>
+            {projectArticleDirectories.map(articleDirectory => (
+              <Route path={`/${articleDirectory}`} element={<MarkdownPage file={`articles/${articleTypes['project']}/${articleDirectory}/post.md`}/>} />
+            ))}
+          </Route>
+          <Route path="/blog" element={<Articles />}>
+            {blogArticleDirectories.map(articleDirectory => (
+              <Route path={`/${articleDirectory}`} element={<MarkdownPage file={`articles/${articleTypes['blog']}/${articleDirectory}/post.md`}/>} />
+            ))}
+          </Route>
         </Routes>
       </div>
     </Router>
--- src/components/article_card_list/functions.js
+++ src/components/article_card_list/functions.js
@@ -1,3 +1,5 @@
+import { getArticleDirectories } from "../../App.js";
+
 export const articleTypes = {
     blog: "blog_articles",
     project: "project_articles"
@@ -16,14 +18,7 @@ const loadArticleMetadata = async (articleType, articleDirectory) => {
 };
 
 export const loadArticleMetadatas = async (articleType) => {
-    const response = await fetch(`/articles/${articleTypes[articleType]}/directories.json`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
-    });
-
-    const articleDirectories = await response.json();
+    const articleDirectories = await getArticleDirectories(articleType);
     const articleMetadatas = await Promise.all(articleDirectories.map(dir => loadArticleMetadata(articleType, dir)));
     return articleMetadatas
 };
